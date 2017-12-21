db52cec54d32d7b3600914dd098ecbee401c4083
Jonathyn Tellez <jbtellez@Jonathyns-MBP.domain>
2017-12-21 11:58:33 -0800

    fix review text and add stretch

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -1,14 +1,22 @@
 'use strict';
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
+  
   /* REVIEW: This is a new construct to save all the properties of rawDataObj into our newly instantiated object. Object.keys is a function that returns an array of all the properties of an object as strings. forEach is an array method that iterates over and calls a function on each element of an array.
   We can also set properties on objects with bracket notation instead of dot notation, which we must do when we don't necessarily know what the property name will be and thus set it as a variable.
-  Additionally, what "this" is changes depending on your context - inside a constructor function, like Article, "this" refers to the newly instantiated object. However, inside the anonymous function we're passing into forEach as an argument, "this" in 'use strict' mode will be undefined. As a result, we can pass our instantiated object "this" into forEach as a second argument to preserve context.
+  Additionally, notice the use of "this" within the arrow function. How does this differ from using a classic function? 
   There is a LOT of new behavior going on here! Review object bracket notation and Object.keys to try and grok what's going on here.*/
   Object.keys(rawDataObj).forEach(key => {
     this[key] = rawDataObj[key];
-  }, this);
+  });
+
+  /* STRETCH: Using Object.keys is a big improvent over explicitly typing each and every key. 
+But did you know that ES6 can do the same thing in one line?
+Research it and give it shot! */
+
 }
+
+
 
 Article.all = [];
 
